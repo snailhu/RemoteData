3216727b6d13af3f67d3f3a4620de9659b37043a
jiazhongjin <jiazj@fengyuntec.com>
2016-09-22 16:20:33 +0800

    routing

---
+++ src/main/java/DataAn/routing/DataSearchTask.java
@@ -0,0 +1,57 @@
+package DataAn.routing;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+import org.bson.Document;
+
+import DataAn.Analysis.dto.YearAndParamDataDto;
+import DataAn.common.utils.DateUtil;
+import DataAn.mongo.db.MongodbUtil;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+
+public class DataSearchTask extends RecursiveTask<YearAndParamDataDto> {
+	
+	private DataSearchTaskConfig dataSearchTaskConfig;
+	
+	public DataSearchTask(DataSearchTaskConfig dataSearchTaskConfig) {
+		super();
+		this.dataSearchTaskConfig = dataSearchTaskConfig;
+	}
+
+	@Override
+	protected YearAndParamDataDto compute() {
+		Repo repo=dataSearchTaskConfig.getRepo();
+		Date startDate=dataSearchTaskConfig.getStartDate();
+		Date endDate=dataSearchTaskConfig.getEndDate();
+		
+		MongodbUtil mg = MongodbUtil.getInstance();
+		MongoCollection<Document> collection = mg.getCollection(repo.database(), repo.collection());		
+
+		YearAndParamDataDto yearAndParam = new YearAndParamDataDto();
+		List<String> yearValue=new ArrayList<String>();
+		List<String> paramValue =  new ArrayList<String>();
+		
+		FindIterable<Document> document_It = collection.find(Filters.and(Filters.gte("datetime", startDate),Filters.lte("datetime", endDate)));						
+		for (Document doc : document_It) {
+			String paraVal = doc.getString(dataSearchTaskConfig.getProperty());
+			if (paraVal != null) {
+				yearValue.add(DateUtil.format(doc.getDate("datetime")));
+				paramValue.add(paraVal);
+			}
+		}
+		yearAndParam.setParamValue(paramValue);
+	    yearAndParam.setYearValue(yearValue);
+		return yearAndParam;
+	}
+
+	public DataSearchTaskConfig getDataSearchTaskConfig() {
+		return dataSearchTaskConfig;
+	}
+	
+}
---
+++ src/main/java/DataAn/routing/MongoFilter.java
@@ -0,0 +1,30 @@
+package DataAn.routing;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MongoFilter implements Serializable {
+	
+	private Date startDate;
+	
+	private Date endDate;
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+
+}

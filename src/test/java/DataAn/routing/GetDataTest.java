62eec0b9799c807b6ac48396dc761f28f99db588
TRhanz <hanz@tongyuan.cc>
2016-11-17 09:04:36 +0800

    漏传文件

---
+++ src/test/java/DataAn/routing/GetDataTest.java
@@ -0,0 +1,24 @@
+package DataAn.routing;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import DataAn.Analysis.dto.ParamAttributeDto;
+import DataAn.Analysis.dto.ParamBatchDto;
+import DataAn.Analysis.dto.YearAndParamDataDto;
+import DataAn.common.utils.DateUtil;
+
+public class GetDataTest {
+	@Test
+	public void test(){
+	
+	
+	}
+
+}

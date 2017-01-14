c84f345082a5abb9f91d27385dabcade586e1c54
Shenwp <Shenwp@Shenwp-PC>
2017-01-14 09:18:22 +0800

    shenwp

--- src/main/java/DataAn/fileSystem/controller/HomeController.java
+++ src/main/java/DataAn/fileSystem/controller/HomeController.java
@@ -2,16 +2,20 @@ package DataAn.fileSystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/new")
 public class HomeController {
 
 	// 返回欢迎登录界面
-	@RequestMapping("/index")
+	@RequestMapping("/new/index")
 	public String index() {
 		return "/admin/new/index";
 	}
 	
-
+	// 返回欢迎登录界面
+	@RequestMapping(value="/newIndex3d", method = { RequestMethod.GET })
+	public String index3d() {
+		return "/index3d";
+	}
 }
